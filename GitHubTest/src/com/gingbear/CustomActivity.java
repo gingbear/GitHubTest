e5c19031eb30663cf22a8f96b62f98abd8a7f399
t-kanda <t-kanda@gmail.com>
2012-05-21 22:44:41 +0900

    ファイルを２つ追加

---
+++ GitHubTest/src/com/gingbear/CustomActivity.java
@@ -0,0 +1,9 @@
+package com.gingbear;
+
+import android.app.Activity;
+
+public class CustomActivity extends Activity  {
+	public static void test(){
+		
+	}
+}
---
+++ GitHubTest/src/com/gingbear/CustomReceiver.java
@@ -0,0 +1,15 @@
+package com.gingbear;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
