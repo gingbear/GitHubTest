4f4a610a1ad81a15de6ce6e782c2df1d0b899eac
t-kanda <gingbear2501@gmail.com>
2012-06-13 00:51:37 +0900

    test

--- GitHubTest/src/com/gingbear/githubtest/receiver/C2DMReceiver.java
+++ GitHubTest/src/com/gingbear/githubtest/receiver/C2DMReceiver.java
@@ -8,26 +8,31 @@ import android.os.Message;
 
 import com.gingbear.githubtest.CustomActivity;
 import com.gingbear.githubtest.CustomLog;
+import com.gingbear.githubtest.GitHubTestActivity;
 import com.google.android.c2dm.C2DMBaseReceiver;
 
 public class C2DMReceiver extends C2DMBaseReceiver{
-	private CustomActivity TopActivity = null;
-	public C2DMReceiver(Context context) {
-		super(context.getString(R.string.googleId));
+//	private CustomActivity TopActivity = null;
+	public C2DMReceiver() {
+//		super(context.getString(R.string.googleId));
+		super("test@gmail.com");
 		// TODO Auto-generated constructor stub
+		CustomLog.w("message:", "test-----");
 	}
 
     // 登録時の処理
     @Override
     public void onRegistered(
 		Context context, String registration_id) {
+    	CustomLog.w("message:", "test1");
     	CustomLog.i("C2DMReceiver#onRegistered", registration_id);
     }
 	@Override
 	protected void onMessage(Context context, Intent intent) {
 		// TODO Auto-generated method stub
+		CustomLog.w("message:", "test2");
 		String str = intent.getStringExtra("message");
-        CustomLog.w("message:", str);
+		CustomLog.w("message:", str);
         sendMessage(str);
 	}
 
@@ -35,21 +40,24 @@ public class C2DMReceiver extends C2DMBaseReceiver{
     // 登録解除時の処理
     @Override
     public void onUnregistered(Context context) {
+    	CustomLog.w("message:", "test3");
         sendMessage("C2DM Unregistered");
     }
     // エラー時の処理
 	@Override
 	public void onError(Context context, String errorId) {
 		// TODO Auto-generated method stub
+		CustomLog.w("message:", "test4");
         sendMessage("err:" + errorId);
 	}
     // サーバーからメッセージを受け取ったときの処理
     private void sendMessage(String str) {
-        Message mes = Message.obtain(TopActivity.mH);
+    	CustomLog.w("message:", "test5");
+        Message mes = Message.obtain(GitHubTestActivity.mH);
         Bundle data = mes.getData();
         data.putBoolean("receivedMessageFlag", true);
         data.putString("receivedMessageString", str);
-        TopActivity.mH.sendMessage(mes);
+        GitHubTestActivity.mH.sendMessage(mes);
         mes = null;
     }
 
