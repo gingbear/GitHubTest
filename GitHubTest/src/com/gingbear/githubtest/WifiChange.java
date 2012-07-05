061404ce94e26054624585adfa27bf10aa2657b0
cyberZ-t-kanda <dimage.t.kanda@gmail.com>
2012-07-05 18:48:01 +0900

    wifi

--- GitHubTest/src/com/gingbear/githubtest/WifiChange.java
+++ GitHubTest/src/com/gingbear/githubtest/WifiChange.java
@@ -1,10 +1,16 @@
 package com.gingbear.githubtest;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.widget.ArrayAdapter;
 
 public class WifiChange {
 	static public String check(Intent intent){
@@ -71,5 +77,64 @@ public class WifiChange {
 			}
 //		}
 //		return "non";
+	}
+	static public String getMacAddress(Context context){
+		WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE); 
+
+		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+
+		String macAddress = wifiInfo.getMacAddress();
+		return macAddress;
+	}
+	static public String getWifiState(Context context){
+		WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+		// android.permission.ACCESS_WIFI_STATE
+		int wifiState = wifiManager.getWifiState();
+		switch (wifiState) {
+		case WifiManager.WIFI_STATE_DISABLING:// 「使用不可にする」をしている状態
+			CustomLog.v("WifiState", "WIFI_STATE_DISABLING");
+		    break;
+		case WifiManager.WIFI_STATE_DISABLED://使用不可
+			CustomLog.v("WifiState", "WIFI_STATE_DISABLED");
+		    break;
+		case WifiManager.WIFI_STATE_ENABLING:// 「使用可能にする」をしている状態
+			CustomLog.v("WifiState", "WIFI_STATE_ENABLING");
+		    break;
+		case WifiManager.WIFI_STATE_ENABLED:// 使用可能
+			CustomLog.v("WifiState", "WIFI_STATE_ENABLED");
+		    break;
+		case WifiManager.WIFI_STATE_UNKNOWN:
+			CustomLog.v("WifiState", "WIFI_STATE_UNKNOWN");
+		    break;
+		}
+		return "";
+	}
+//	WifiConfiguration#getメソッドで, 設定情報を取り出す.
+//	以下の情報(一部)が取得できる.
+//	BSSID	AP( アクセス・ポイント)のMACアドレス
+//	SSID	ネットワーク名
+//	hiddenSSID	ステルスモード
+//	networkId	ネットワーク番号
+//	preSharedKey	WPA-PSKの鍵
+//	priority	複数のAPが使用できるときの優先度
+//	status	ネットワークの状態
+//	wepKeys	WEPキー
+//	wepTxKeyIndex	WEPキーのインデックス
+	static public String getConfiguredNetworks(Context context){
+		WifiManager manager  = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+		// WiFi設定の一覧を取得
+		List<WifiConfiguration> cfgList = manager.getConfiguredNetworks();
+		if(cfgList != null) {
+			String[] nets = new String[cfgList.size()];
+			for(int i=0; i<cfgList.size(); i++) {
+				nets[i] = String.format("Network ID:%4d\nSSID:%s", 
+													cfgList.get(i).networkId, cfgList.get(i).SSID);
+			}
+//			ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nets);
+//			setListAdapter(adapter);
+		}
+		return "";
 	}
 }
